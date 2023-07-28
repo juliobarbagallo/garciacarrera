fee06dbc72956ce82e753be6620216855111b055
julio cesar barbagallo <julio.barbagallo@gmail.com>
2023-07-28 16:25:54 -0300

    adds contact form interaction

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* config options here */
-    output: 'standalone',
-  }
-   
-  module.exports = nextConfig
+// const nextConfig = {
+//     /* config options here */
+//     output: 'standalone',
+//   }
+
+//   module.exports = nextConfig
+
+module.exports = {
+  env: {
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    SERVICE_ID: process.env.SERVICE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+  },
+};
